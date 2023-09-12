9c528b8b687f9ae8969294a918d3eb6a0d85b2ed
Roberto Carlos <robertocarlos.cortes@dgeti.sems.gob.mx>
2023-09-12 15:03:02 -0600

    Subiendo

--- authorization-server/src/main/java/com/tutorial/authorizationserver/controller/AuthController.java
+++ authorization-server/src/main/java/com/tutorial/authorizationserver/controller/AuthController.java
@@ -6,6 +6,7 @@ import com.tutorial.authorizationserver.service.AppUserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthController {
     private final AppUserService appUserService;
+    @PostMapping("/create")
     public ResponseEntity<MessageDTO> createUser(@RequestBody CreateAppUserDTO dto) {
         return ResponseEntity.status(HttpStatus.CREATED).body(appUserService.createUser(dto));
     }
